fead7b4e69f46ef1e9971dbb3c0147562f82a430
Larissa Gon�alves Santos <42220054870@>
2025-03-12 16:58:26 -0300

    atualizado

--- front/src/components/modal/index.jsx
+++ front/src/components/modal/index.jsx
@@ -1,30 +1,30 @@
 import React, {useEffect, useState} from "react";
 import './styles.css'
+import axios from "axios";
 
 const ModalProfessores = ({
     isOpen,
     onClose,
     professorSelecionado,
-    criar,
-    atualizar
 })=>{
     if(!isOpen) return null
 
-    const [id, setId] = useState(professorSelecionado?.id || '')
-    const [ni, setNi] = useState(professorSelecionado?.ni || '')
-    const [nome, setNome] = useState(professorSelecionado?.nome || '')
-    const [email, setEmail] = useState(professorSelecionado?.email || '')
-    const [tel, setTel] = useState(professorSelecionado?.tel || '')
-    const [ocupacao, setOcupacao] = useState(professorSelecionado?.ocupacao || '')
+    const [id, setId] = useState(professorSelecionado?.id ?? '')
+    const [ni, setNi] = useState(professorSelecionado?.ni ?? '')
+    const [nome, setNome] = useState(professorSelecionado?.nome ?? '')
+    const [email, setEmail] = useState(professorSelecionado?.email ?? '')
+    const [tel, setTel] = useState(professorSelecionado?.tel ?? '')
+    const [ocupacao, setOcupacao] = useState(professorSelecionado?.ocupacao ?? '')
+    const token = localStorage.getItem('token')
 
     useEffect(()=>{
         if(professorSelecionado){
-            setId(professorSelecionado.id || '')
-            setNi(professorSelecionado.ni || '')
-            setNome(professorSelecionado.nome || '')
-            setEmail(professorSelecionado.email || '')
-            setTel(professorSelecionado.tel || '')
-            setOcupacao(professorSelecionado.ocupacao || '')
+            setId(professorSelecionado.id ?? '')
+            setNi(professorSelecionado.ni ?? '')
+            setNome(professorSelecionado.nome ?? '')
+            setEmail(professorSelecionado.email ?? '')
+            setTel(professorSelecionado.tel ?? '')
+            setOcupacao(professorSelecionado.ocupacao ?? '')
         }else{
             setId('')
             setNi('')
@@ -45,14 +45,42 @@ const ModalProfessores = ({
             criar(novoProfessor)
         }
     }
+    
+    const newTeacher = async() =>{
+        console.log("chegoi")
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/professores',
+            {
+                ni: ni,
+                nome: nome,
+                email: email,
+                tel: tel,
+                ocupacao: ocupacao
+
+            },{
+                    headers:{
+                        Autorization: `Bearer` ${token}
+                    }
+                }
+            }
+        
+        console.log("Professor inserido com sucesso")
+        onclose(true)
+        } catch (error) {
+            
+        }
+    }
 
     return(
         <div className="modal-modal">
             <div className="container-modal">
-                <button className="close-button" onClick={onClose}>X</button>
+                <div className="head-modal">
+                    <button className="close-button" onClick={onClose}>X</button>
+                </div>
                 <h2>{professorSelecionado ? "Editar" : "Cadastrar"}</h2>
                 <div className="body-modal">
                     <form onSubmit={handleSubmit}>
+                        <div className="caixa-1">
                         <input
                             className="ni-modal"
                             value={ni}
@@ -83,9 +111,16 @@ const ModalProfessores = ({
                             placeholder="ocupacao"
                             onChange={(e)=>setOcupacao(e.target.value)}
                         />
-                        <button type="submit">Salvar</button> 
-                    </form>
-                </div>
+                        </div>
+                            <div className="caixa2">
+
+                        </div>
+                        </form>
+                    </div>
+                    <div className="footer-modal">
+                        <button type="submit" className="button-save">Salvar</button> 
+                    </div>
+                
             </div>
         </div>
     )
